468e570832b3922306a2bb62273d358355a48c3c
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-06-29 23:24:42 +0430

    add navigation for menu-item with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
+
+function Hats(props) {
+  console.log(props);
+  return <h1>Hats</h1>;
+}
+// function Topics(props) {
+//   console.log(1, props);
+//   return (
+//     <>
+//       <h1>Topics {props.match.params.id}</h1>
+//       <Link to="/topics/33">Topic 33</Link>
+//       <Link to="/">Topic 33</Link>
+//       <Link to={`${props.match.url}//15`}>15</Link>
+//       <button onClick={() => props.history.push("/topics/38")}>
+//         Topics 38
+//       </button>
+//       <button onClick={() => props.history.goBack()}>back</button>
+//       <button onClick={() => props.history.goForward()}>Forward</button>
+//     </>
+//   );
+// }
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/shop/hats">
+          <Hats />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.componets.jsx
+++ src/components/menu-item/menu-item.componets.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const history = useHistory();
+  const { url } = useRouteMatch();
+
+  return (
     <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
