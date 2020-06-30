ab5dbfd3b3f60ae78c7e661c8f5a16f4909cfe68
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-07-01 00:05:39 +0430

    add shop page, cokkection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+// Components
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route, Link } from "react-router-dom";
-
+import ShopPage from "./pages/shop/shop.component";
+// CSS
 import "./App.css";
-
-function Hats(props) {
-  console.log(props);
-  return <h1>Hats</h1>;
-}
-// function Topics(props) {
-//   console.log(1, props);
-//   return (
-//     <>
-//       <h1>Topics {props.match.params.id}</h1>
-//       <Link to="/topics/33">Topic 33</Link>
-//       <Link to="/">Topic 33</Link>
-//       <Link to={`${props.match.url}//15`}>15</Link>
-//       <button onClick={() => props.history.push("/topics/38")}>
-//         Topics 38
-//       </button>
-//       <button onClick={() => props.history.goBack()}>back</button>
-//       <button onClick={() => props.history.goForward()}>Forward</button>
-//     </>
-//   );
-// }
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/shop/hats">
-          <Hats />
+        <Route path="/shop">
+          <ShopPage />
         </Route>
         <Route path="/">
           <HomePage />
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...rest }) => (
+          <CollectionPreview key={id} {...rest} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
